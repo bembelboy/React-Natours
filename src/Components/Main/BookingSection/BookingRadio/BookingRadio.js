a1d2dc181d3acb2ba165336da7f4947fc47127ee
bembelboy <58267287+bembelboy@users.noreply.github.com>
2020-06-02 17:15:15 +0200

    Styling RadioButtons

--- src/Components/Main/BookingSection/BookingRadio/BookingRadio.js
+++ src/Components/Main/BookingSection/BookingRadio/BookingRadio.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, {
+  useState
+} from 'react';
 
 import * as styled from './styledBookingRadio.js';
 
 const BookingRadio = (props) => {
+  const [Selected, setSelected] = useState(false);
+  console.log(Selected)
+
+  const onSelectedChange = (event) => {
+    console.log(event.target.className)
+    setSelected(event.target.className)
+  }
+
   return (
     <React.Fragment>
       <styled.RadioFormBox>
-        <styled.RadioInput  type='radio' name='GroupSize'/>
-        <styled.RadioLabel>Small Tour Group</styled.RadioLabel>
+        <styled.RadioInput  type='radio' name='GroupSize' className='small' id='small'
+
+        />
+        <styled.RadioLabel htmlFor='small' className='small'>
+
+          <styled.FormRadioButton></styled.FormRadioButton>
+          Small Tour Group
+        </styled.RadioLabel>
       </styled.RadioFormBox>
       <styled.RadioFormBox>
-        <styled.RadioInput type='radio' name='GroupSize' />
-        <styled.RadioLabel>Large Tour Group</styled.RadioLabel>
+        <styled.RadioInput type='radio' name='GroupSize' className='large' id='large' />
+
+
+          <styled.RadioLabel htmlFor='large' className='large'>
+
+          <styled.FormRadioButton></styled.FormRadioButton>
+          Large Tour Group
+        </styled.RadioLabel>
       </styled.RadioFormBox>
     </React.Fragment>
   );
--- src/Components/Main/BookingSection/BookingRadio/styledBookingRadio.js
+++ src/Components/Main/BookingSection/BookingRadio/styledBookingRadio.js
@@ -1,12 +1,48 @@
 import styled from 'styled-components';
 
-export const RadioFormBox = styled.div `
-`;
+import * as nameProperty from './../../../../GlobalStyles/Theme/nameProperties.js';
 
-export const RadioInput = styled.input`
-display: inline;
+export const RadioFormBox = styled.div `
+  width: 50%;
+  display: inline-block;
 `;
 
 export const RadioLabel = styled.label`
+  font-size: ${props => props.theme[nameProperty.defaultFontSize]};
+  cursor: pointer;
+  position: relative;
+  padding-left: 5rem;
+`;
 
+export const FormRadioButton = styled.span`
+  height: 3rem;
+  width: 3rem;
+  border: 5px solid rgb(${props => props.theme[nameProperty.mediumGreen]});
+  border-radius: 50%;
+  display: inline-block;
+  position: absolute;
+  left: 0;
+  top: -.5rem;
+
+&::after {
+  content: "";
+  display: block;
+  height: 1.3rem;
+  width: 1.3rem;
+  border-radius: 50%;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgb(${props => props.theme[nameProperty.mediumGreen]});
+  opacity: 0;
+  transition: opacity .2s;
+}
+`;
+
+export const RadioInput = styled.input`
+  display: none;
+&:checked ~ ${RadioLabel} ${FormRadioButton}:: after {
+opacity: 1;
+}
 `;
